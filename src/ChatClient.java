453ed1e4b0abee97e996e9faa80d49984ad442b6
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2022-12-28 23:01:44 +0000

    Client - now conecting to the server

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -4,9 +4,15 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 
 public class ChatClient {
+
+    // enviroment thing - disable this to not see any debug screens
+    private static final boolean Debug = true;
 
     // Variáveis relacionadas com a interface gráfica --- * NÃO MODIFICAR *
     JFrame frame = new JFrame("Chat Client");
@@ -16,9 +22,10 @@ public class ChatClient {
 
     // Se for necessário adicionar variáveis ao objecto ChatClient, devem
     // ser colocadas aqui
-
-
-
+    private static final int bufferSize = 16384;
+    private final ByteBuffer outBuffer = ByteBuffer.allocate(bufferSize);
+    private final ByteBuffer inBuffer = ByteBuffer.allocate(bufferSize);
+    private SocketChannel Socket = null;
 
     // Método a usar para acrescentar uma string à caixa de texto
     // * NÃO MODIFICAR *
@@ -29,7 +36,6 @@ public class ChatClient {
 
     // Construtor
     public ChatClient(String server, int port) throws IOException {
-
         // Inicialização da interface gráfica --- * NÃO MODIFICAR *
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JPanel panel = new JPanel();
@@ -62,28 +68,33 @@ public class ChatClient {
 
         // Se for necessário adicionar código de inicialização ao
         // construtor, deve ser colocado aqui
-
-
-
+        
+        Socket = SocketChannel.open(new InetSocketAddress(server, port));
     }
 
 
     // Método invocado sempre que o utilizador insere uma mensagem
-    // na caixa de entrada
+    // na daixa de entrada
     public void newMessage(String message) throws IOException {
-        // PREENCHER AQUI com código que envia a mensagem ao servidor
+        if(Debug) System.out.println(message);
 
+        //add termination chars to string
+        message = message + "\n\0";
+        
+        //clear the buffer and prepare message for transmition
+        outBuffer.clear();
+        outBuffer.put(message.getBytes());
+        outBuffer.flip();
 
-
+        //while message is not fully sent, send
+        while(outBuffer.hasRemaining())
+            Socket.write(outBuffer); 
     }
 
 
     // Método principal do objecto
     public void run() throws IOException {
         // PREENCHER AQUI
-
-
-
     }
 
 
